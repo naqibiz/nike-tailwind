4bb4265d4a165ded34df04ad9ac08a3445767643
Muhammad Naqi <naqimng786@gmail.com>
2023-10-18 06:58:46 -0700

    Initial commit

---
+++ src/components/ShoseCard.jsx
@@ -0,0 +1,29 @@
+const ShoseCard = ({ imgUrl, changeBigShoseImage, BigShoseImage }) => {
+  const handleClick = () => {
+    if (BigShoseImage !== imgUrl.bigShoe) {
+      changeBigShoseImage(imgUrl.bigShoe);
+    }
+  };
+  return (
+    <div
+      className={`border-2 rounded-xl ${
+        BigShoseImage === imgUrl.bigShoe
+          ? "border-coral-red"
+          : "border-transparent"
+      } cursor-pointer max-sm:flex-1`}
+      onClick={handleClick}
+    >
+      <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+        <img
+          src={imgUrl.thumbnail}
+          alt="shose collection"
+          width={127}
+          height={103}
+          className="object-contain"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ShoseCard;
